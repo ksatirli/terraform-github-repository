b8431b4db885e5a3edb11136ea834ab40f77616b
Kerim Satirli <k.satirli@gmail.com>
2020-03-05 13:15:51 +0100

    updates outputs for `github_repository` to HCL 2.0 and adds descriptions

--- output.tf
+++ output.tf
@@ -1,22 +1,30 @@
-##
-# outputs for `github_repository`
-##
 output "full_name" {
-  value = "${github_repository.repository.full_name}"
+  value       = github_repository.repository.full_name
+  description = " A string of the form \"orgname/reponame\""
+}
+
+output "html_url" {
+  value       = github_repository.repository.ssh_clone_url
+  description = "URL to the repository on the web"
 }
 
 output "ssh_clone_url" {
-  value = "${github_repository.repository.ssh_clone_url}"
+  value       = github_repository.repository.ssh_clone_url
+  description = "URL to the repository to clone via SSH"
 }
 
 output "http_clone_url" {
-  value = "${github_repository.repository.http_clone_url}"
+  value       = github_repository.repository.http_clone_url
+  description = "URL to clone the repository via HTTPs"
 }
 
 output "git_clone_url" {
-  value = "${github_repository.repository.git_clone_url}"
+  value       = github_repository.repository.git_clone_url
+  description = "URL to clone the repository via the git protocol"
 }
 
 output "svn_url" {
-  value = "${github_repository.repository.svn_url}"
+  value       = github_repository.repository.svn_url
+  description = "URL to check out the repository via GitHub's Subversion protocol emulation"
 }
+
