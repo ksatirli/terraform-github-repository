160e76d3a519a1e0c3f49f3ae446c75c3a3aea5e
Kerim Satirli <k.satirli@gmail.com>
2020-06-25 10:52:34 +0200

    updates resource and output names to `this`

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "repository" {
+resource "github_repository" "this" {
   name               = var.name
   description        = var.description
   homepage_url       = var.homepage_url
@@ -27,10 +27,10 @@ resource "github_repository" "repository" {
   }
 }
 
-resource "github_branch_protection" "branch_protection" {
+resource "github_branch_protection" "this" {
   count = length(var.branch_protections)
 
-  repository             = github_repository.repository.name
+  repository             = github_repository.this.name
   branch                 = var.branch_protections[count.index].branch
   enforce_admins         = var.branch_protections[count.index].enforce_admins
   require_signed_commits = var.branch_protections[count.index].require_signed_commits
@@ -56,52 +56,52 @@ resource "github_branch_protection" "branch_protection" {
 
 // TODO: add support for https://www.terraform.io/docs/providers/github/r/repository_webhook.html
 
-resource "github_repository_deploy_key" "deploy_key" {
+resource "github_repository_deploy_key" "this" {
   count = length(var.deploy_keys)
 
   title      = var.deploy_keys[count.index].title
-  repository = github_repository.repository.name
+  repository = github_repository.this.name
   key        = var.deploy_keys[count.index].key
   read_only  = var.deploy_keys[count.index].read_only
 }
 
-resource "github_repository_collaborator" "collaborator" {
+resource "github_repository_collaborator" "this" {
   count = length(var.repository_collaborators)
 
-  repository = github_repository.repository.name
+  repository = github_repository.this.name
   username   = var.repository_collaborators[count.index].username
   permission = lookup(var.repository_collaborators[count.index], "permission", "push")
 }
 
-resource "github_team_repository" "team_repository" {
+resource "github_team_repository" "this" {
   count = length(var.team_repository_teams)
 
   team_id    = var.team_repository_teams[count.index].team_id
-  repository = github_repository.repository.name
+  repository = github_repository.this.name
   permission = lookup(var.team_repository_teams[count.index], "permission", "push")
 }
 
-resource "github_issue_label" "issue_label" {
+resource "github_issue_label" "this" {
   count = length(var.issue_labels)
 
-  repository  = github_repository.repository.name
+  repository  = github_repository.this.name
   name        = var.issue_labels[count.index].name
   color       = var.issue_labels[count.index].color
   description = lookup(var.issue_labels[count.index], "description", null)
 }
 
-resource "github_repository_project" "project" {
+resource "github_repository_project" "this" {
   count = length(var.projects)
 
   name       = var.projects[count.index].name
-  repository = github_repository.repository.name
+  repository = github_repository.this.name
   body       = var.projects[count.index].body
 }
 
-resource "github_repository_file" "file" {
+resource "github_repository_file" "this" {
   count = length(var.files)
 
-  repository = github_repository.repository.name
+  repository = github_repository.this.name
   file       = var.files[count.index].file
   content    = var.files[count.index].content
   branch     = lookup(var.files[count.index], "branch", "master")
@@ -110,3 +110,12 @@ resource "github_repository_file" "file" {
   commit_email   = lookup(var.files[count.index], "email", null)
   commit_message = lookup(var.files[count.index], "message", null)
 }
+
+///TODO: add support for `github_actions_secret`
+//resource "github_actions_secret" "secret" {
+//  count = length(var.secrets)
+//
+//  repository       = github_repository.repository.name
+//  secret_name      = var.secrets[count.index].name
+//  plaintext_value  = var.secrets[count.index].plaintext_value
+//}
--- output.tf
+++ output.tf
@@ -1,55 +1,55 @@
 output "full_name" {
-  value       = github_repository.repository.full_name
+  value       = github_repository.this.full_name
   description = " A string of the form \"orgname/reponame\""
 }
 
 output "html_url" {
-  value       = github_repository.repository.ssh_clone_url
+  value       = github_repository.this.ssh_clone_url
   description = "URL to the repository on the web"
 }
 
 output "ssh_clone_url" {
-  value       = github_repository.repository.ssh_clone_url
+  value       = github_repository.this.ssh_clone_url
   description = "URL to the repository to clone via SSH"
 }
 
 output "http_clone_url" {
-  value       = github_repository.repository.http_clone_url
+  value       = github_repository.this.http_clone_url
   description = "URL to clone the repository via HTTPs"
 }
 
 output "git_clone_url" {
-  value       = github_repository.repository.git_clone_url
+  value       = github_repository.this.git_clone_url
   description = "URL to clone the repository via the git protocol"
 }
 
 output "svn_url" {
-  value       = github_repository.repository.svn_url
+  value       = github_repository.this.svn_url
   description = "URL to check out the repository via GitHub's Subversion protocol emulation"
 }
 
 // TOOD: define a good way to make this useful for consumption
 //output "repository_collaborator_invitation_id" {
-//  value       = github_repository_collaborator.collaborator[*].invitation_id
+//  value       = github_repository_collaborator.this[*].invitation_id
 //  description = "ID of the invitation to be used in `github_user_invitation_accepter`"
 //}
 
 output "project_ids" {
-  value       = github_repository_project.project[*].id
+  value       = github_repository_project.this[*].id
   description = "List of Repository Project IDs"
 }
 
 output "project_urls" {
-  value       = github_repository_project.project[*].url
+  value       = github_repository_project.this[*].url
   description = "List of Repository Project IDs"
 }
 
 output "projects" {
-  value       = zipmap(github_repository_project.project[*].id, github_repository_project.project[*].url)
+  value       = zipmap(github_repository_project.this[*].id, github_repository_project.this[*].url)
   description = "Map of Repository Project IDs, and corresponding URLs"
 }
 
 output "files" {
-  value       = zipmap(github_repository_file.file[*].file, github_repository_file.file[*].sha)
+  value       = zipmap(github_repository_file.this[*].file, github_repository_file.this[*].sha)
   description = "Map of Repository File names and corresponding SHA blobs"
 }
