f460a5a8a48fe6b119f5fb5f78ca046d331b453b
Kerim Satirli <k.satirli@gmail.com>
2020-06-25 11:40:16 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 resource "github_repository" "this" {
-  name               = var.name
-  description        = var.description
-  homepage_url       = var.homepage_url
-  private            = var.private
-  has_issues         = var.has_issues
-  has_projects       = var.has_projects
-  has_wiki           = var.has_wiki
-  is_template        = var.is_template
-  allow_merge_commit = var.allow_merge_commit
-  allow_squash_merge = var.allow_squash_merge
-  allow_rebase_merge = var.allow_rebase_merge
+  name                   = var.name
+  description            = var.description
+  homepage_url           = var.homepage_url
+  private                = var.private
+  has_issues             = var.has_issues
+  has_projects           = var.has_projects
+  has_wiki               = var.has_wiki
+  is_template            = var.is_template
+  allow_merge_commit     = var.allow_merge_commit
+  allow_squash_merge     = var.allow_squash_merge
+  allow_rebase_merge     = var.allow_rebase_merge
   delete_branch_on_merge = var.delete_branch_on_merge
-  has_downloads      = var.has_downloads
-  auto_init          = var.auto_init
-  gitignore_template = var.gitignore_template
-  license_template   = var.license_template
-  default_branch     = var.default_branch
-  archived           = var.archived
-  topics             = var.topics
+  has_downloads          = var.has_downloads
+  auto_init              = var.auto_init
+  gitignore_template     = var.gitignore_template
+  license_template       = var.license_template
+  default_branch         = var.default_branch
+  archived               = var.archived
+  topics                 = var.topics
 
   dynamic "template" {
     for_each = length(var.template) != 0 ? [var.template] : []
