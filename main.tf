d59f12e27e55ddcb61e84892913ab6a962396ba2
Kerim Satirli <k.satirli@gmail.com>
2022-08-22 13:47:56 +0200

    renames resources from `this` to `main` and updates outputs

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-resource "github_repository" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository
+resource "github_repository" "main" {
   name         = var.name
   description  = var.description
   homepage_url = var.homepage_url
-
-  # `private` has been deprecated in favor of `visibility`
   visibility = var.visibility
-
   has_issues             = var.has_issues
   has_projects           = var.has_projects
   has_wiki               = var.has_wiki
@@ -18,9 +16,6 @@ resource "github_repository" "this" {
   auto_init              = var.auto_init
   gitignore_template     = var.gitignore_template
   license_template       = var.license_template
-
-  # `default_branch` has been deprecated in favor of a `github_branch_default` resource
-
   archived           = var.archived
   archive_on_destroy = var.archive_on_destroy
 
@@ -49,85 +44,94 @@ resource "github_repository" "this" {
   vulnerability_alerts = var.vulnerability_alerts
 }
 
-resource "github_branch_default" "this" {
-  repository = github_repository.this.name
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/branch_default
+resource "github_branch_default" "main" {
+  repository = github_repository.main.name
   branch     = var.default_branch
 }
 
-resource "github_branch_protection_v3" "this" {
-  count = length(var.branch_protections)
+# TODO
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/branch_protection
+#resource "github_branch_protection" "main" {
+#  count = length(var.branch_protections)
+#
+#  repository             = github_repository.main.name
+#  branch                 = var.branch_protections[count.index].branch
+#  enforce_admins         = var.branch_protections[count.index].enforce_admins
+#  require_signed_commits = var.branch_protections[count.index].require_signed_commits
+#
+#  required_status_checks {
+#    strict   = lookup(lookup(var.branch_protections[count.index], "required_status_checks", null), "strict", null)
+#    contexts = lookup(lookup(var.branch_protections[count.index], "required_status_checks", null), "contexts", null)
+#  }
+#
+#  required_pull_request_reviews {
+#    dismiss_stale_reviews           = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "dismiss_stale_reviews", null)
+#    dismissal_users                 = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "dismissal_users", null)
+#    dismissal_teams                 = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "dismissal_teams", null)
+#    require_code_owner_reviews      = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "require_code_owner_reviews", null)
+#    required_approving_review_count = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "required_approving_review_count", null)
+#  }
+#
+#  restrictions {
+#    users = lookup(lookup(var.branch_protections[count.index], "restrictions", null), "users", null)
+#    teams = lookup(lookup(var.branch_protections[count.index], "restrictions", null), "teams", null)
+#  }
+#}
 
-  repository             = github_repository.this.name
-  branch                 = var.branch_protections[count.index].branch
-  enforce_admins         = var.branch_protections[count.index].enforce_admins
-  require_signed_commits = var.branch_protections[count.index].require_signed_commits
-
-  required_status_checks {
-    strict   = lookup(lookup(var.branch_protections[count.index], "required_status_checks", null), "strict", null)
-    contexts = lookup(lookup(var.branch_protections[count.index], "required_status_checks", null), "contexts", null)
-  }
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "dismiss_stale_reviews", null)
-    dismissal_users                 = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "dismissal_users", null)
-    dismissal_teams                 = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "dismissal_teams", null)
-    require_code_owner_reviews      = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "require_code_owner_reviews", null)
-    required_approving_review_count = lookup(lookup(var.branch_protections[count.index], "required_pull_request_reviews", null), "required_approving_review_count", null)
-  }
-
-  restrictions {
-    users = lookup(lookup(var.branch_protections[count.index], "restrictions", null), "users", null)
-    teams = lookup(lookup(var.branch_protections[count.index], "restrictions", null), "teams", null)
-  }
-}
-
-resource "github_repository_deploy_key" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository_deploy_key
+resource "github_repository_deploy_key" "main" {
   count = length(var.deploy_keys)
 
   key        = var.deploy_keys[count.index].key
   read_only  = var.deploy_keys[count.index].read_only
-  repository = github_repository.this.name
+  repository = github_repository.main.name
   title      = var.deploy_keys[count.index].title
 }
 
-resource "github_repository_collaborator" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository_collaborator
+resource "github_repository_collaborator" "main" {
   count = length(var.repository_collaborators)
 
-  repository                  = github_repository.this.name
+  repository                  = github_repository.main.name
   username                    = var.repository_collaborators[count.index].username
   permission                  = lookup(var.repository_collaborators[count.index], "permission", "push")
   permission_diff_suppression = lookup(var.repository_collaborators[count.index], "permission_diff_suppression", false)
 }
 
-resource "github_team_repository" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/team_repository
+resource "github_team_repository" "main" {
   count = length(var.team_repository_teams)
 
   team_id    = var.team_repository_teams[count.index].team_id
-  repository = github_repository.this.name
+  repository = github_repository.main.name
   permission = lookup(var.team_repository_teams[count.index], "permission", "push")
 }
 
-resource "github_issue_label" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/issue_label
+resource "github_issue_label" "main" {
   count = length(var.issue_labels)
 
-  repository  = github_repository.this.name
+  repository  = github_repository.main.name
   name        = var.issue_labels[count.index].name
   color       = var.issue_labels[count.index].color
   description = lookup(var.issue_labels[count.index], "description", null)
 }
 
-resource "github_repository_project" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository_project
+resource "github_repository_project" "main" {
   count = length(var.projects)
 
   name       = var.projects[count.index].name
-  repository = github_repository.this.name
+  repository = github_repository.main.name
   body       = var.projects[count.index].body
 }
 
-resource "github_repository_file" "this" {
+# see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository_file
+resource "github_repository_file" "main" {
   count = length(var.files)
 
-  repository = github_repository.this.name
+  repository = github_repository.main.name
   file       = var.files[count.index].file
   content    = var.files[count.index].content
   branch     = lookup(var.files[count.index], "branch", "main")
--- output.tf
+++ output.tf
@@ -1,70 +1,52 @@
-output "name" {
-  value       = github_repository.this.name
-  description = "A string of the form \"reponame\""
+output "github_repository" {
+  description = "Exported Attributes for `github_repository`."
+  value       = github_repository.main
 }
 
-output "full_name" {
-  value       = github_repository.this.full_name
-  description = "A string of the form \"orgname/reponame\""
+output "github_branch_default" {
+  description = "Exported Attributes for `github_branch_default`."
+  value       = github_branch_default.main
 }
 
-output "html_url" {
-  value       = github_repository.this.ssh_clone_url
-  description = "URL to the repository on the web"
+output "github_branch_protection_v3" {
+
+  description = "Exported Attributes for `github_branch_protection_v3`."
+  value       = github_branch_protection_v3.main
 }
 
-output "ssh_clone_url" {
-  value       = github_repository.this.ssh_clone_url
-  description = "URL to the repository to clone via SSH"
+output "github_repository_deploy_key" {
+  description = "Exported Attributes for `github_repository_deploy_key`."
+  value       = github_repository_deploy_key.main
 }
 
-output "http_clone_url" {
-  value       = github_repository.this.http_clone_url
-  description = "URL to clone the repository via HTTPs"
+output "github_repository_collaborator" {
+  description = "Exported Attributes for `github_repository_collaborator`."
+  value       = github_repository_collaborator.main
 }
 
-output "git_clone_url" {
-  value       = github_repository.this.git_clone_url
-  description = "URL to clone the repository via the git protocol"
+output "github_team_repository" {
+  description = "Exported Attributes for `github_team_repository`."
+  value       = github_team_repository.main
 }
 
-output "svn_url" {
-  value       = github_repository.this.svn_url
-  description = "URL to check out the repository via GitHub's Subversion protocol emulation"
+output "github_issue_label" {
+  description = "Exported Attributes for `github_issue_label`."
+  value       = github_issue_label.main
 }
 
-output "node_id" {
-  value       = github_repository.this.node_id
-  description = "Node ID of the Repository"
+output "github_repository_project" {
+  description = "Exported Attributes for `github_repository_project`."
+  value       = github_repository_project.main
 }
 
-output "repo_id" {
-  value       = github_repository.this.name
-  description = "ID of the Repository"
+output "github_repository_file" {
+  description = "Exported Attributes for `github_repository_file`."
+  value       = github_repository_file.main
 }
 
 # TOOD: define a good way to make this useful for consumption
-#output "repository_collaborator_invitation_id" {
-#  value       = github_repository_collaborator.this[*].invitation_id
-#  description = "ID of the invitation to be used in `github_user_invitation_accepter`"
-#}
-
-output "project_ids" {
-  value       = github_repository_project.this[*].id
-  description = "List of Repository Project IDs"
+output "repository_collaborator_invitation_ids" {
+  description = "ID of the invitation to be used in `github_user_invitation_accepter`."
+  value       = github_repository_collaborator.main[*].invitation_id
 }
 
-output "project_urls" {
-  value       = github_repository_project.this[*].url
-  description = "List of Repository Project IDs"
-}
-
-output "projects" {
-  value       = zipmap(github_repository_project.this[*].id, github_repository_project.this[*].url)
-  description = "Map of Repository Project IDs, and corresponding URLs"
-}
-
-output "files" {
-  value       = zipmap(github_repository_file.this[*].file, github_repository_file.this[*].sha)
-  description = "Map of Repository File names and corresponding SHA blobs"
-}
