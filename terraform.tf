99c136e49f54ed90705401617d565347703c1fa3
Kerim Satirli <k.satirli@gmail.com>
2022-08-22 13:08:03 +0200

    explicitly reference the correct GitHub Provider
    Co-Authored-By: Steven Cooney <30004860+theyorkshiredev@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,13 @@
 terraform {
-  required_version = ">= 0.14.0"
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
+  required_providers {
+    # see https://registry.terraform.io/providers/integrations/github/4.29.0
+    github = {
+      source  = "integrations/github"
+      version = "4.29.0"
+    }
+  }
+
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
+  required_version = ">= 1.0.0"
 }
