df50266de2b38be5af3d26c3fc2b12e9cc71737d
Kerim Satirli <k.satirli@gmail.com>
2022-08-22 12:14:00 +0200

    brings projects code in line with examples of other modules

---
+++ examples/project/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
+  required_providers {
+    # see https://registry.terraform.io/providers/integrations/github/4.29.0
+    github = {
+      source  = "integrations/github"
+      version = "4.29.0"
+    }
+  }
+
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
+  required_version = ">= 1.0.0"
+}
