71d55c7c1dd787894a5778d65240357d5e636e59
Kerim Satirli <k.satirli@gmail.com>
2022-09-26 15:44:35 +0200

    bumps provider version to `5.3.0` from `5.2.0`

--- examples/template/terraform.tf
+++ examples/template/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/5.2.0
+    # see https://registry.terraform.io/providers/integrations/github/5.3.0
     github = {
       source  = "integrations/github"
-      version = "5.2.0"
+      version = ">= 5.3.0, < 6.0.0"
     }
   }
 
