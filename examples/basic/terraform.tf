3f10ef3b439ea55a1e93197ab5fd705bb57be08a
Kerim Satirli <k.satirli@gmail.com>
2025-02-28 18:09:31 +0100

    updates GitHub Provider from `6.2.1` to `6.6.0`

--- examples/basic/terraform.tf
+++ examples/basic/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   # see https://developer.hashicorp.com/terraform/language/terraform#terraform-required_providers
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/6.2.1
+    # see https://registry.terraform.io/providers/integrations/github/6.6.0
     github = {
       source  = "integrations/github"
-      version = ">= 6.2.1, < 7.0.0"
+      version = ">= 6.6.0, < 7.0.0"
     }
   }
 
