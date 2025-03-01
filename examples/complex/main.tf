15a574a09396175f3df2d0e3bfe3bd082e148bee
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-03-01 01:20:09 +0000

    add web_commit_signoff_required example

--- examples/complex/main.tf
+++ examples/complex/main.tf
@@ -1,20 +1,21 @@
 module "complex_example" {
   source = "../.."
 
-  name               = "complex-example"
-  description        = "Complex Repository Example"
-  homepage_url       = "https://github.com/ksatirli"
-  visibility         = "private"
-  has_issues         = true
-  has_projects       = true
-  has_wiki           = false
-  allow_merge_commit = false
-  allow_squash_merge = true
-  allow_rebase_merge = false
-  has_downloads      = false
-  auto_init          = true
-  default_branch     = "main"
-  archived           = false
+  name                        = "complex-example"
+  description                 = "Complex Repository Example"
+  homepage_url                = "https://github.com/ksatirli"
+  visibility                  = "private"
+  has_issues                  = true
+  has_projects                = true
+  has_wiki                    = false
+  allow_merge_commit          = false
+  allow_squash_merge          = true
+  allow_rebase_merge          = false
+  has_downloads               = false
+  auto_init                   = true
+  default_branch              = "main"
+  archived                    = false
+  web_commit_signoff_required = true
 
   topics = [
     "topic-1",
